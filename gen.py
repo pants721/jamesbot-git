ab59601552000ed4e511456f0751b6a2da8c6174
Lucas Newcomb <94380471+pants721@users.noreply.github.com>
2022-04-11 10:05:28 -0400

    Initial commit

---
+++ gen.py
@@ -0,0 +1,159 @@
+import csv
+import random
+
+import nltk
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words('english'))
+
+phraseList = []
+authorList = []
+tokList = []
+taggedList = ""
+
+symbolBlackList = [",", ".", "`", "'", '"', "?", "!", "(", ")"]
+
+nounList = []
+advList = []
+adjList = []
+verbList = []
+pronList = []
+numList = []
+adpList = []
+
+# NOUN = NOUN
+# ADVERB = ADV
+# ADJECTIVE = ADJ
+# VERB = VERB
+# PRONOUN = PRON
+is_noun = lambda pos: pos[:4] == 'NOUN'
+is_adv = lambda pos: pos[:3] == 'ADV'
+is_adj = lambda pos: pos[:3] == 'ADJ'
+is_verb = lambda pos: pos[:4] == 'VERB'
+is_pron = lambda pos: pos[:4] == 'PRON'
+is_num = lambda pos: pos[:3] == 'NUM'
+is_adp = lambda pos: pos[:3] == 'ADP'
+
+def genLists():
+    """Generates two lists: phraseList and authorList from csv file.
+    
+    Stores a string arr of the sentences in phraseList.
+    Stores a string arr of the authors for the sentences at corresponding indecies 
+    in authorList.
+
+    Ex:
+    Quote: "Hi, my name is Dan!", Dan
+    phraseList[__index] = "Hi, my name is Dan!"
+    authorList[__index] = "Dan"
+    """
+    with open('phrases.csv', 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            phraseList.insert(len(phraseList), row[0])
+            authorList.insert(len(authorList), row[1])
+
+def tokenizeList(list):
+    """Tokenizes a list.
+
+    Args:
+        list (string array): a list of strings.
+    """
+    for x in list:
+        tokens = nltk.word_tokenize(x)
+        # Removes stop words (ex: the, is, and)
+        tokens = [w for w in tokens if not w in stop_words]
+        for i in tokens:
+            # removes any list items that contain a symbol in symbolBlackList 
+            if any(c in i for c in symbolBlackList):
+                break
+            else:
+                tokList.insert(len(tokList), i)
+    cleanList(tokList)
+
+def cleanList(list):
+    """Function to remove any list items that are only a quotation to account for weird csv thing
+
+    Args:
+        list (string array): Tokenized List
+    """
+    for i in range(len(tokList)):
+        try:
+            list.remove('“')
+            list.remove('”')
+        except ValueError:
+            pass
+
+def sortList(list):
+    """ Sorts a tagged list using universal taglist into parts of speech in their 
+    respective arrays
+
+    Args:
+        list (sorted list): A sorted list of tokens
+    """
+    # POS IS PART OF SPEECH NOT POSITION ! 
+    for (word, pos) in list:
+        if is_noun(pos):
+            nounList.insert(len(nounList), word)
+        elif is_adv(pos):
+            advList.insert(len(advList), word)
+        elif is_adj(pos):
+            adjList.insert(len(adjList), word)
+        elif is_verb(pos):
+            verbList.insert(len(verbList), word)
+        elif is_pron(pos):
+            pronList.insert(len(pronList), word)
+        elif is_num(pos):
+            numList.insert(len(numList), word)
+        elif is_adp(pos):
+            adpList.insert(len(adpList), word)
+        else:
+            # DEBUG
+            # print("Bad input")
+            # print(word, pos)
+            pass
+
+# basic
+def testSentenceGen(length):
+    """ Basic function that generates sentences using a few basic sentence structures
+    with words chosen from random indecies of the POS arrays
+
+    Args:
+        length (int): the number of words in the sentence
+
+    Returns:
+        string: The generated sentence
+    """
+    if length == 3:
+        randChoice = random.randint(0, 1)
+        if randChoice == 0:
+            # Pronoun Verb Noun
+            pronRandWord = pronList[random.randint(0, len(pronList)) - 1]
+            verbRandWord = verbList[random.randint(0, len(verbList)) - 1]
+            nounRandWord = nounList[random.randint(0, len(nounList)) - 1]
+            return f'{pronRandWord} {verbRandWord} {nounRandWord}.'
+        elif randChoice == 1:
+            # Noun Verb Noun2
+            nounRandWord = nounList[random.randint(0, len(nounList)) - 1]
+            verbRandWord = verbList[random.randint(0, len(verbList)) - 1]
+            nounRandWord2 = nounList[random.randint(0, len(nounList)) - 1]
+            return f'{nounRandWord} {verbRandWord} {nounRandWord2}.'
+    elif length == 4:
+            # Noun ADP Noun2 Verb
+            nounRandWord = nounList[random.randint(0, len(nounList)) - 1]
+            adpRandWord = adpList[random.randint(0, len(adpList)) - 1]
+            nounRandWord2 = nounList[random.randint(0, len(nounList)) - 1]
+            verbRandWord = verbList[random.randint(0, len(verbList)) - 1]
+            return f'{nounRandWord} {adpRandWord} {nounRandWord2} {verbRandWord}.'
+
+
+
+
+def generateSentence():
+    genLists()
+    tokenizeList(phraseList)
+    taggedList = nltk.pos_tag(tokList, tagset="universal")
+    sortList(taggedList)
+    randChoiceNum = random.randint(3, 4)
+    return testSentenceGen(randChoiceNum).lower()
+            
+generateSentence()
---
+++ main.py
@@ -0,0 +1,128 @@
+import random
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+from dotenv import load_dotenv
+import os
+import os.path
+import csv
+import gen
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+bot = commands.Bot(command_prefix='$')
+
+phraseList = []
+authorList = []
+
+nonoWords = []
+
+
+@bot.event
+async def on_ready():
+    print('{0.user} is online.'.format(bot))
+
+
+def generateLists():
+    with open('phrases.csv', 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            phraseList.insert(len(phraseList), row[0])
+            authorList.insert(len(authorList), row[1])
+
+
+@bot.command()
+async def wacky(ctx):
+    generateLists()
+    csvNum = random.randint(0, len(phraseList) - 2)
+    await ctx.channel.send(f'{phraseList[csvNum]} -{authorList[csvNum]}')
+
+
+def writeToCsv(quote, author):
+    with open('phrases.csv', 'a', newline='') as csvfile:
+        write = csv.writer(csvfile)
+        csvfile.write("\n")
+        write.writerow([quote, f' {author}'])
+
+
+@commands.has_permissions(administrator=True)
+@bot.command()
+async def quote(ctx, quote, author):
+    charArr = list(quote)
+    if charArr[0] != '"':
+        quote = f'"{quote}"'
+    with open('phrases.csv', 'a', newline="") as csvfile:
+        write = csv.writer(csvfile)
+        read = csv.writer(csvfile)
+        csvfile.write("\n")
+        csvfile.write(f'""{quote}"", {author}')
+    generateLists()
+    await ctx.channel.send(f'Quote from {author} has been recorded.')
+
+
+@commands.has_permissions(administrator=True)
+@bot.command(pass_context=True)
+async def listAll(ctx):
+    generateTxt()
+    lines = []
+    with open("quotes.txt", "r",
+              encoding='utf-8') as file:  # Use this to open and close the file
+        for line in file.readlines():
+            line = line.strip()
+            lines.append(line)
+            index1 = 1
+            index1 += 1
+            if index1 == (len(lines) + 1):
+                break
+
+    await ctx.send(file=discord.File('quotes.txt'))
+
+
+def generateTxt():
+    generateLists()
+    with open("quotes.txt", "w", encoding='utf-8') as f:
+        for j in range(0, len(authorList)):
+            f.write(f'{phraseList[j]}, {authorList[j]}\n')
+
+
+@bot.command()
+async def listPerson(ctx, person):
+    generateLists()
+    dictionary = dict(zip(phraseList, authorList))
+    embed = discord.Embed()
+    for quote, author in dictionary.items():
+        if author.lower() == f' {person.lower()}':
+            embed.add_field(name=f'{author}', value=f'{quote}', inline=False)
+    await ctx.channel.send(embed=embed)
+
+
+@bot.command()
+async def last(ctx):
+    generateLists()
+    await ctx.channel.send(
+        f'{phraseList[len(phraseList) - 1]} -{authorList[len(authorList) - 1]}'
+    )
+
+
+@bot.command()
+async def oops(ctx):
+    with open('advikStats.txt', 'r') as f:
+        lines = f.readlines()
+        count = int(lines[0])
+        days = lines[1]
+    with open('advikStats.txt', 'w') as f:
+        count += 1
+        days = "0"
+        f.write(f'{str(count)}\n')
+        f.write(days)
+    await ctx.channel.send("Stats have been updated")
+
+
+@bot.command()
+async def sentence(ctx):
+    await ctx.channel.send(gen.generateSentence())
+
+
+generateLists()
+bot.run(TOKEN)
